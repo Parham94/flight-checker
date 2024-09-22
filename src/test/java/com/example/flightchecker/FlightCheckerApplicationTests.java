b0dc8a6ec1b7e9cd312bb560e4c560ab967b69ba
parham94 <Parhamtbb@gmail.com>
2024-09-22 14:42:55 -0400

    Added code base

---
+++ src/test/java/com/example/flightchecker/FlightCheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.flightchecker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightCheckerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
