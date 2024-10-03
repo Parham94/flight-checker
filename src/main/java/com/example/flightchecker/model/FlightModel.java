476d948bbc33c61a2c31c1b08a7dc0a808b71cca
parham Tabibi <parhamtbb@gmail.com>
2024-10-02 22:38:04 -0400

    updating Model class

--- src/main/java/com/example/flightchecker/model/FlightModel.java
+++ src/main/java/com/example/flightchecker/model/FlightModel.java
@@ -3,12 +3,17 @@ package com.example.flightchecker.model;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.Date;
+import java.util.Objects;
+
 //// Lombok Annotations
 @Data
 @Setter
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
+@EqualsAndHashCode
+@ToString
 @Builder // read on this
 //// JPA Annotation
 @Entity
@@ -21,4 +26,23 @@ public class FlightModel {
 
     @Column(unique = true, nullable = false)
     private String flightNo;
+
+    @Column(unique = true, nullable = false)
+    private String source;
+
+    @Column(unique = true, nullable = false)
+    private String destination;
+
+    @Column(unique = true, nullable = false)
+    private Date departureTime;
+
+    @Column(unique = true, nullable = false)
+    private Date arrivalTime;
+
+    @Column(unique = true, nullable = false)
+    private String airline;
+
+    @Column(unique = true, nullable = false)
+    private int numSeats;
+
 }
